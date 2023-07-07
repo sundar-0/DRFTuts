0b2586c31841183d7de7d743e05eb88e49c3a10f
sundar-0 <sundardumre69@gmail.com>
2023-07-07 15:00:50 +0545

    DRF practices

---
+++ tutorial/tutorial/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+from django.contrib.auth import admin
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    #path('admin/', admin.site.urls)
+    # path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls'))
+]
